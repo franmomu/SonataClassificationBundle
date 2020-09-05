9130e5e8ef0909a827ea1486873257fa233eb6eb
Fran Moreno <franmomu@gmail.com>
2020-09-06 00:54:18 +0200

    fix master test

--- tests/App/Entity/Category.php
+++ tests/App/Entity/Category.php
@@ -14,8 +14,7 @@ declare(strict_types=1);
 namespace Sonata\ClassificationBundle\Tests\App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
-use Sonata\ClassificationMediaBundle\Entity\BaseCategory;
-use Sonata\MediaBundle\Model\MediaInterface;
+use Sonata\ClassificationBundle\Entity\BaseCategory;
 
 /**
  * @ORM\Entity
@@ -30,23 +29,8 @@ class Category extends BaseCategory
      */
     protected $id;
 
-    /**
-     * @var MediaInterface|null
-     */
-    private $media;
-
     public function getId(): int
     {
         return $this->id;
-    }
-
-    public function setMedia(?MediaInterface $media = null): void
-    {
-        $this->media = $media;
-    }
-
-    public function getMedia(): ?MediaInterface
-    {
-        return $this->media;
     }
 }
